ed9b5543cc8b0c99e735a3a4ded91d23bb8b41ba
mehrabdarvishi <soidontknow23@gmail.com>
2024-02-10 23:24:03 +0330

    unnecessary utility functions were removed from utils.py

--- backend/schedules/utils.py
+++ backend/schedules/utils.py
@@ -1,8 +1,1 @@
-from .models import Semester, Program
-
-def get_semester(semester_code):
-    semester_qs = Semester.objects.filter(code=semester_code)
-    if  semester_qs.exists():
-        return semester_qs.first()
-    return None
 
