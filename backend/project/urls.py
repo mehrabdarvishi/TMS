0abb186afa6fb2ab1c4a2b77ea913e0c0e1025c8
Mehrab Darvishi <soidontknow23@gmail.com>
2024-02-17 15:10:09 +0330

    base app

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('schedules/', include('schedules.urls'))
+    path('schedules/', include('schedules.urls')),
 ]
