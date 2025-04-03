f88d6e78a27baacd9f374787738cf9a2be7b4682
mehrabdarvishi <soidontknow23@gmail.com>
2025-04-04 01:42:41 +0330

    index page url update

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('schedules/', include('schedules.urls')),
+    path('', include('schedules.urls')),
     path('reports/', include('reports.urls')),
 ]
